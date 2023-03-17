b2371ca8d2f4628f38341ad1c82993be410c3c7a
mhao <mhao@odoo.com>
2023-03-17 10:27:37 +0100

    split in functions

--- src/main/java/xml_rpc/XML_RPC.java
+++ src/main/java/xml_rpc/XML_RPC.java
@@ -57,7 +57,50 @@ public class XML_RPC {
 
 
             //List of instances search and read specific fields
-            /*List list_instances_search_read = new ArrayList<>();
+            xmlRpc.listSearchReadInstance(db, uid, password, models);
+
+            //Creation of instances
+            final Integer id = xmlRpc.createInstance(db, uid, password, models);
+            System.out.println("=====> Instance created " + id);
+            xmlRpc.listSearchReadInstance(db, uid, password, models);
+
+            //Update instance
+            xmlRpc.updateInstance(db, uid, password, models, id);
+            //get instance name after the update
+            List list_instance = asList((Object[])models.execute("execute_kw", asList(
+                    db, uid, password,
+                    "instance.request", "name_get",
+                    asList(asList(id))
+            )));
+            System.out.println("=====> Instance modified " + id);
+            System.out.println(list_instance + "\n");
+
+            //Delete instance
+            xmlRpc.deleteInstance(db, uid, password, models, id);
+            //check if the deleted instance is still in the database
+            System.out.println("=====> Instance deleted ");
+            asList((Object[])models.execute("execute_kw", asList(
+                    db, uid, password,
+                    "instance.request", "search",
+                    asList(asList(asList("id", "=", id)))
+            )));
+            xmlRpc.listSearchReadInstance(db, uid, password, models);
+
+        }
+    }
+
+    public void displayListInstance(String db, int uid, String password, XmlRpcClient models) throws XmlRpcException {
+        List list_instances = new ArrayList<>();
+        list_instances = asList((Object[]) models.execute("execute_kw", asList(
+                db, uid, password,
+                "instance.request", "search",
+                asList(asList())
+        )));
+        System.out.println(list_instances);
+    }
+
+    public void listSearchReadInstance(String db, int uid, String password, XmlRpcClient models) throws XmlRpcException {
+        List list_instances_search_read = new ArrayList<>();
             list_instances_search_read = asList((Object[]) models.execute("execute_kw", asList(
                     db, uid, password,
                     "instance.request", "search_read",
@@ -67,65 +110,29 @@ public class XML_RPC {
                     //    put("limit", 5);
                     }}
             )));
-            System.out.println(list_instances_search_read);*/
-
-            //Creation of instances
-            final Integer id = (Integer) models.execute("execute_kw", asList(
-                    db, uid, password,
-                    "instance.request", "create",
-                    asList(new HashMap() {{
-                        put("name", "Serveur FX");
-                        put("limit_date", "2023-04-25");
-                        put("state", "submitted");
-                    }})
-            ));
-            System.out.println("=====> Instance created " + id);
-            xmlRpc.displayListIntance(db, uid, password, models);
-
-            //Update instance
-            models.execute("execute_kw", asList(
-                    db, uid, password,
-                    "instance.request", "write",
-                    asList(
-                            asList(id),
-                            new HashMap() {{
-                                put("state", "draft");
-                            }}
-                    )
-            ));
-            //get instance name after the update
-            List list_instance = asList((Object[])models.execute("execute_kw", asList(
-                    db, uid, password,
-                    "instance.request", "name_get",
-                    asList(asList(id))
-            )));
-
-            System.out.println(list_instance);
-
-            //Delete instance
-            models.execute("execute_kw", asList(
-                    db, uid, password,
-                    "instance.request", "unlink",
-                    asList(asList(id))
-            ));
-
-            //check if the deleted instance is still in the database
-            asList((Object[])models.execute("execute_kw", asList(
-                    db, uid, password,
-                    "instance.request", "search",
-                    asList(asList(asList("id", "=", id)))
-            )));
-
-        }
+            System.out.println(list_instances_search_read + "\n");
     }
 
-    public void displayListIntance(String db, int uid, String password, XmlRpcClient models) throws XmlRpcException {
-        List list_instances = new ArrayList<>();
-        list_instances = asList((Object[]) models.execute("execute_kw", asList(
-                db, uid, password,
-                "instance.request", "search",
-                asList(asList())
-        )));
-        System.out.println(list_instances);
+    public Integer createInstance(String db, int uid, String password, XmlRpcClient models) throws XmlRpcException {
+        Integer id = (Integer)models.execute("execute_kw", List.of(db, uid, password, "instance.request", "create", List.of(new HashMap() {
+            {
+                this.put("name", "Serveur FX");
+                this.put("limit_date", "2023-04-25");
+                this.put("state", "submitted");
+            }
+        })));
+        return id;
+    }
+
+    public void updateInstance(String db, int uid, String password, XmlRpcClient models, int id) throws XmlRpcException {
+        models.execute("execute_kw", List.of(db, uid, password, "instance.request", "write", List.of(List.of(id), new HashMap() {
+            {
+                this.put("state", "draft");
+            }
+        })));
+    }
+
+    public void deleteInstance(String db, int uid, String password, XmlRpcClient models, int id) throws XmlRpcException {
+        models.execute("execute_kw", List.of(db, uid, password, "instance.request", "unlink", List.of(List.of(id))));
     }
 }
